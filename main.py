d441431df8592fde991de0bedd7c130bdc5a2120
Syed Ghaneim Ahmed <gha770isawesome@gmail.com>
2024-10-17 22:05:28 +0300

    Auto Clicker Added

---
+++ main.py
@@ -0,0 +1,36 @@
+import keyboard as kb
+import time as t
+import pyautogui as mouse
+import threading
+
+click = False
+
+def clicker():
+    while True:
+        if click:
+            mouse.click()
+        t.sleep(0.01)
+        
+def toggle():
+    global click
+    click = not click
+    if click:
+        print("Clicker enabled")
+    else:
+        print("Clicker disabled")
+
+def exit():
+    global click
+    click = False
+    print("Exiting...")
+
+    
+click_thread = threading.Thread(target=clicker)
+click_thread.daemon = True
+click_thread.start()
+
+kb.add_hotkey("left alt+`", toggle)
+kb.add_hotkey("left ctrl+`", exit)
+
+print("Press [left alt+`] to enable/disable clicker\n Press [left ctrl + ` ] to exit")
+kb.wait()
